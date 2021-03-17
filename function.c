7e7d0cadea517adf1081c9ebce68bce7ec94b05c
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-17 00:26:33 +0000

    Fixed Betty issues

--- function.c
+++ function.c
@@ -1,14 +1,22 @@
 #include "holberton.h"
-
+/**
+ * print_char - print characters
+ * @data: data variable
+ * Return: 1
+ */
 int print_char(va_list data)
 {
-    int character = (char)va_arg(data, int);
+	int character = (char)va_arg(data, int);
 
-    	write(1, &character, 1);
+	write(1, &character, 1);
 
 	return (1);
 }
-
+/**
+ * print_string - print strings
+ * @string: string
+ * Return: 1
+ */
 int print_string(va_list string)
 {
 	char *str_char = va_arg(string, char *);
@@ -18,7 +26,11 @@ int print_string(va_list string)
 
 	return (count);
 }
-
+/**
+ * print_porcent - print strings
+ * @porcent: percentage
+ * Return: 1
+ */
 int print_porcent(va_list porcent)
 {
 	(void)porcent;
