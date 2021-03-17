ffe6d1e44256e3d9683d49628db90c69f32083d0
mareizaa <marce.3210@gmail.com>
2021-03-16 22:52:27 -0500

    Correction Betty

--- function.c
+++ function.c
@@ -6,18 +6,17 @@
  */
 int print_char(va_list data)
 {
-    int character = (char)va_arg(data, int);
+	int character = (char)va_arg(data, int);
 
-    if (character != 0)
-    {
-        write(1, &character, 1);
-
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (character != 0)
+	{
+		write(1, &character, 1);
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 /**
  * print_string - print strings
@@ -26,22 +25,20 @@ int print_char(va_list data)
  */
 int print_string(va_list string)
 {
-    char *str = va_arg(string, char *);
+	char *str = va_arg(string, char *);
 
-    if (str && *str)
-    {
-        int count = strlen(str);
+	if (str && *str)
+	{
+		int count = strlen(str);
 
-        write(1, str, count);
-
-        return (count);
-    }
-    else
-    {
-        write(1, "(null)", 6);
-
-        return (6);
-    }
+		write(1, str, count);
+		return (count);
+	}
+	else
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
 }
 
 /**
@@ -54,8 +51,5 @@ int print_porcent(va_list porcent)
 	(void)porcent;
 
 	write(1, "%", 1);
-
 	return (1);
-
-	
 }
