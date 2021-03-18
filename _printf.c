5f4634dbc478751615fb5267bf0a353224fa9f30
ferchislopez <ferchislopez0910@gmail.com>
2021-03-18 15:44:10 -0500

    Correction n

--- _printf.c
+++ _printf.c
@@ -24,6 +24,13 @@ int _printf(const char *format, ...)
 			{
 				count += func_ptr(data);
 			}
+			else
+			{
+				write(1, format - 1, 1);
+				count++;
+				write(1, format, 1);
+				count++;
+			}
 			if (*format != '%')
 			{
 				format++;
