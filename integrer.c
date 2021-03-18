f4135e8494db6a236433a58c6ed23c7f927a9a46
mareizaa <marce.3210@gmail.com>
2021-03-17 22:11:22 -0500

    Correction

--- integrer.c
+++ integrer.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+unsigned int get_absolut(int num);
 /**
  * print_integrer - prints integers
  * @number: integer
@@ -9,27 +10,26 @@ int print_integrer(va_list number)
 	int num = va_arg(number, int);
 	long digit = 1, divider10 = 10, divider = 10;
 	long condition = 1, cont = 0, num_print = 0;
+	unsigned int absolut = get_absolut(num);
 
 	if (num < 0)
-	{	num = -1 * num;
-		write(1, "-", 1);
+	{	write(1, "-", 1);
 		cont++;
 	}
-	if (num >= 10)
+	if (absolut >= 10)
 	{
 		while (condition)
 		{
-			while ((num / divider) > 10)
-			{
-				divider10 *= 10;
+			while ((absolut / divider) >= 10)
+			{	divider10 *= 10;
 				divider = divider10;
 			}
 			if (digit == 1)
-			{	num_print = (num / divider10);
+			{	num_print = (absolut / divider10);
 				digit = 0;
 			}
 			else
-			{	num_print = (num / divider10);
+			{	num_print = (absolut / divider10);
 				num_print %= 10;
 			}
 			num_print = num_print + '0';
@@ -41,9 +41,24 @@ int print_integrer(va_list number)
 		}
 	}
 	else
-	{	num = num + '0';
-		write(1, &num, 1);
+	{	absolut = absolut + '0';
+		write(1, &absolut, 1);
 		cont++;
 	}
 	return (cont);
+}
+/**
+ * get_absolut - absolut number
+ * @num: integer
+ * Return: return absolut number
+ */
+unsigned int get_absolut(int num)
+{
+	unsigned int absolut;
+
+	if (num < 0)
+		absolut = ((unsigned int)(num)) * -1;
+	else
+		absolut = ((unsigned int)(num));
+	return (absolut);
 }
